22d691240a82934192a40e8d6fb34bbfed8621e6
xi_jjun <rlawowns97@naver.com>
2022-05-03 06:17:49 +0900

    refactor: add patch, post method for posting or updating Posting Entity
    Line:26-28
    log 를 보기 위해 Sl4j 추가.
    update posting method실행을 위해 @Transactional 추가.
    PostingController에 들어오는 요청 url은 '/community/postings' 로 통일.
    
    findPostings method 수정
    return type을 PostingResonseListDTO로 바꿈. 요청/응답에 대한 DTO객체를 확실히 클래스 이름으로
    구분하기 위함.
    
    findPositing method 추가
    postingId에 대해 게시물 자세한 조회 진행. 반환은 PostingResponseDetailDTO로 반환.
    
    posting method 추가
    게시물 작성을 위한 기능. PostingRequestDTO에 사용자의 입력을 담아서 Posting Entity를 만든 후,
    DB에 저장을 한다.
    현재 사용자 정보는 나중에 session에서 구할 것이기에 null값으로 대체하여 넣어줬다.
    
    반환은 ReponseDTO를 하나 만들어서 게시물이 잘 작성되었는지를 알 수 있게 해줬다.
    
    updatePosting method 추가
    postingId에 해당하는 게시물을 수정하는 기능.
    
    인가된 사용자(게시글을 작성한 사람)만이 수정이 가능하게 향후 구현해야 한다.

--- src/main/java/board/project/community/controller/PostingController.java
+++ src/main/java/board/project/community/controller/PostingController.java
@@ -1,50 +1,102 @@
 package board.project.community.controller;
 
+import board.project.community.controller.dto.request.PostingRequestDTO;
+import board.project.community.controller.dto.response.PostingResponseDetailDTO;
+import board.project.community.controller.dto.response.PostingResponseListDTO;
+import board.project.community.domain.board.Board;
 import board.project.community.domain.posting.Posting;
 import board.project.community.domain.posting.PostingDTO;
-import board.project.community.domain.Status;
-import board.project.community.domain.posting.PostingSearchDTO;
+import board.project.community.domain.user.User;
+import board.project.community.repository.BoardRepository;
+import board.project.community.repository.PostingRepository;
+import board.project.community.repository.UserRepository;
 import board.project.community.service.PostingService;
-import lombok.Builder;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
+@Transactional(readOnly = true)
+@RequestMapping("/community/postings")
 @RequiredArgsConstructor
 @RestController
 public class PostingController {
-	private final PostingService postingService;
 
-	@GetMapping("/postings")
-	public List<PostingSearchDTO> findPostings() {
-		List<Posting> postings = postingService.findAllPostings();
-		List<PostingSearchDTO> list = postings.stream()
-				.map(PostingSearchDTO::new)
+	private final BoardRepository boardRepository;
+	private final UserRepository userRepository;
+	private final PostingRepository postingRepository;
+
+	@GetMapping("")
+	public List<PostingResponseListDTO> findPostings() {
+		List<Posting> postings = postingRepository.findAll();
+		return postings.stream()
+				.map(PostingResponseListDTO::new)
 				.collect(Collectors.toList());
-		return list;
 	}
 
-	@GetMapping("/postings/{postingIdx}")
-	public PostingDTO findPosting(@PathVariable("postingIdx") String postingIdx) {
-		Long idx = Long.parseLong(postingIdx);
-		Posting findPosting = postingService.findByIdx(idx);
-		return Posting.convertToDTO(findPosting);
+	@GetMapping("/{postingId}")
+	public PostingResponseDetailDTO findPosting(@PathVariable("postingId") String postingId) {
+		Long id = Long.parseLong(postingId);
+		Posting findPosting = postingRepository.findById(id);
+		return new PostingResponseDetailDTO(findPosting);
+	}
+
+	@PostMapping("")
+	public ResponseDTO posting(@RequestBody PostingRequestDTO postingRequestDTO) {
+		// user 얻고,
+		String boardName = postingRequestDTO.getBoardName();
+		Board board = boardRepository.findByName(boardName);
+
+		Posting posting = postingRequestDTO.toEntity();
+		posting.setBoard(board);
+
+		// session, security 써서 거기서 찾아서 넣어줘야 함.
+		posting.setUser(null);
+
+		postingRepository.save(posting);
+
+		return new ResponseDTO("posting is ok", 200);
+	}
+
+	@Transactional
+	@PatchMapping("/{postingId}")
+	public ResponseDTO updatePosting(@RequestBody PostingRequestDTO postingRequestDTO,
+									 @PathVariable String postingId) {
+		Long id = Long.parseLong(postingId);
+		Posting findPosting = postingRepository.findById(id);
+		findPosting.update(postingRequestDTO);
+
+		/**
+		 * 없는 board name 에 대해서 예외처리 필요함. 향후 구현
+		 * 게시물 작성자는 안바뀌므로 신경 x
+		 */
+		String substitutionBoardName = postingRequestDTO.getBoardName();
+		Board substitutionBoard = boardRepository.findByName(substitutionBoardName);
+		findPosting.setBoard(substitutionBoard);
+
+		return new ResponseDTO("good", 200);
 	}
 
 	/**
-	 * DTO to Entity mapping 부터 하자!!!
-	 * 그리고 controller 완성하자
+	 * userController 에 있는게 맞는 것 같다
 	 */
-//	@PatchMapping("/postings/{postingsIdx}")
-//	public String updatePosting(@RequestBody PostingDTO posting) {
-//		postingService.updatePosting(posting);
+//	@GetMapping("/nickname/{userNickname}")
+//	public List<PostingSearchDTO> findAllUserPostings(@PathVariable("userNickname") String userNickname) {
+//		List<Posting> postings = postingService.findByNickname(userNickname);
+//		return postings.stream()
+//				.map(PostingSearchDTO::new)
+//				.collect(Collectors.toList());
 //	}
-
 }

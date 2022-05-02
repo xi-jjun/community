df07b21e61ac3d955545f35bb86c3beadfa38208
xi_jjun <rlawowns97@naver.com>
2022-05-03 06:21:20 +0900

    feat: add PostingResponseListDTO for showing all postings list

---
+++ src/main/java/board/project/community/controller/dto/response/PostingResponseListDTO.java
@@ -0,0 +1,31 @@
+package board.project.community.controller.dto.response;
+
+import board.project.community.domain.posting.Posting;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class PostingResponseListDTO {
+	private Long id;
+	private String title;
+	private String writer;
+	private LocalDateTime createdDate;
+	private String boardName;
+
+	public PostingResponseListDTO(Posting posting) {
+		this.id = posting.getId();
+		this.title = posting.getTitle();
+		this.writer = posting.getUser().getNickname();
+		this.createdDate = getCreationDate(posting);
+		this.boardName = posting.getBoard().getName();
+	}
+
+	private LocalDateTime getCreationDate(Posting posting) {
+		if (posting.getUpdatedDate() == null) {
+			return posting.getCreatedDate();
+		}
+
+		return posting.getUpdatedDate();
+	}
+}

d25a9714eb00c17f2d13adf0ebfe177d19590ccf
xi_jjun <rlawowns97@naver.com>
2022-05-03 05:06:58 +0900

    feat: add Posting DTO classes for request and response Posting entity by using DTO
    DTO 로 굳이 요청/응답으로 나눠서 생성한 이유
    내가 원하는 API format으로 요청과 응답을 주고 싶었기 때문. 기획 단계에서 정해진 양식대로 만든 후 필요하다면
    향후 수정할 계획. 처음 설계하는 API라 중복이 다소 있더라도 명확하게 하나씩 완성해가며 만들 것이다.
    
    PostingRequestDTO
    게시글을 쓰기 위해 POST 요청을 했을 때, 사용자로부터 데이터를 받아서 해당 객체에 데이터를 담아 controller에 전달.
    
    PostingResponseDetailDTO
    게시글 단건을 조회할 때 DB에서 찾은 정보를 해당 객체에 담아서 사용자에게 전달.
    	향후 추가 수정이 필요 : 댓글, 좋아요 개수

---
+++ src/main/java/board/project/community/controller/dto/request/PostingRequestDTO.java
@@ -0,0 +1,35 @@
+package board.project.community.controller.dto.request;
+
+import board.project.community.domain.Status;
+import board.project.community.domain.posting.Posting;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * 게시물을 작성할 때 사용자가 보내는 요청 형식
+ */
+@Builder(builderMethodName = "postingRequestBuilder")
+@Data
+public class PostingRequestDTO {
+	private String title;
+	private String subtitle;
+	private String content;
+	private String boardName; // Controller 에를 get method 로 꺼내서 board 에 저장해야함
+
+	/**
+	 * POST 요청을 위해 있다.
+	 * @return : postingRepository 에 저장하기 위한 Posting Entity
+	 */
+	public Posting toEntity() {
+		Posting posting = Posting.PostingBuilder()
+				.title(this.title)
+				.subtitle(this.subtitle)
+				.content(this.content)
+				.status(Status.ACTIVE)
+				.createdDate(LocalDateTime.now())
+				.build();
+		return posting;
+	}
+}
---
+++ src/main/java/board/project/community/controller/dto/response/PostingResponseDetailDTO.java
@@ -0,0 +1,44 @@
+package board.project.community.controller.dto.response;
+
+import board.project.community.domain.Comment;
+import board.project.community.domain.posting.Posting;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class PostingResponseDetailDTO {
+	private String boardName;
+	private String title;
+	private String subtitle;
+	private String content;
+	private LocalDateTime createdDate;
+
+	private Long userId;
+	private String writer;
+
+//	private Preference preference; // like/dislike
+
+	private List<Comment> comments = new ArrayList<>();
+
+	public PostingResponseDetailDTO(Posting posting) {
+		this.boardName = posting.getBoard().getName();
+		this.title = posting.getTitle();
+		this.subtitle = posting.getSubtitle();
+		this.content = posting.getContent();
+		this.createdDate = getCreationDate(posting);
+
+//		this.userId = posting.getUser().getId();
+//		this.writer = posting.getUser().getNickname();
+	}
+
+	private LocalDateTime getCreationDate(Posting posting) {
+		if (posting.getUpdatedDate() == null) {
+			return posting.getCreatedDate();
+		}
+
+		return posting.getUpdatedDate();
+	}
+}

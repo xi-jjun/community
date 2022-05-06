873b62a64d4b3a38d4d7269443977fd8f0670d96
xi_jjun <rlawowns97@naver.com>
2022-05-06 20:49:25 +0900

    refactor: modify User entity column and add method, constructor for updating
    @NoArgsContructor annotation 추가. 생성자를 추가했기에 기본생성자가 1개 필요했어서 추가했다.
    => 기본 생성자가 필요한 이유는, JPA가 내부적으로 Entity를 생성할 수 있어야 하기 때문이라고만 간단하게 알고 있다.
    따라서 기본 생성자를 정의 안해주면 오류가 떠서 만들었다.
    
    DB PK column name 변경. user_idx -> user_id
    이전에 Posting entity 바꿀 때에도 설명을 했었지만, PK라는 속성의 의미는 idx보단 id게 더 잘 맞는다고 판단하여 변경.
    
    field name 변경. id -> account
    PK column name field를 id로 정선언했기에 다른 이름을 써줬어야 했다. 사용자의 계정이라는 뜻이기에 account 라는 이름을 사용.
    
    field 추가. createdDate
    사용자 가입날짜에 대한 정보를 저장하고 싶어서 field를 추가했다.
    
    FK field 3개 추가
    사용자가 쓴 모든 게시물을 조회할 수 있게 하기 위해서 postings field 역참조.
    사용자가 쓴 모든 댓글을 조회할 수 있게 하기 위해서 comments field 역참조.
    사용자가 좋아요한 목록을 보기 위해서 preferenceList field 역참조.
    
    contructor 추가. User 생성시 DTO객체를 User객체로 손쉽게 바꾸기 위함.
    Builder 를 사용할까 했지만, DTO만으로 깔끔하게 정리가 가능했기에 생성자로 받게 함.
    
    updateRole method : 관리자만이 쓸 수 있는 method. 사용자의 권한을 바꿀 수 있다.
    
    update method : 사용자가 본인의 계정에 대한 정보를 수정할 때 쓰이는 method. 찾아온 user entity에 대해 update기능을 하나의
    transaction에서 수행하면 dirty checking에 의해 update query가 DB에 날라간다.

--- src/main/java/board/project/community/domain/user/User.java
+++ src/main/java/board/project/community/domain/user/User.java
@@ -1,7 +1,11 @@
 package board.project.community.domain.user;
 
+import board.project.community.controller.dto.request.UserRequestDTO;
+import board.project.community.domain.Comment;
+import board.project.community.domain.Posting;
+import board.project.community.domain.preference.Preference;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,20 +14,24 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-@Setter // temp
 @Getter
+@NoArgsConstructor
 @Table(name = "Users")
 @Entity
 public class User {
 	@Id
-	@Column(name = "user_idx")
+	@Column(name = "user_id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long idx;
+	private Long id;
 
 	@Column
-	private String id;
+	private String account;
 
 	@Column
 	private String password;
@@ -38,13 +46,52 @@ public class User {
 	private int age;
 
 	@Column
+	private LocalDateTime createdDate;
+
+	@Column
 	@Enumerated(EnumType.STRING)
 	private Role role;
 
+	@OneToMany(mappedBy = "user")
+	private List<Posting> postings = new ArrayList<>();
+
+	@OneToMany(mappedBy = "user")
+	private List<Comment> comments = new ArrayList<>();
+
+	@OneToMany(mappedBy = "user")
+	private List<Preference> preferenceList = new ArrayList<>();
+
 	/**
-	 * 필요없어짐. 왜? ROLE class 에서 BLOCKED 를 판단해주기 때문.
+	 * User 생성
+	 * @param userRequestDTO : 회원가입을 할 회원 정보가 담겨있는 객체
 	 */
-//	@Column
-//	@Enumerated(EnumType.STRING)
-//	private Status status;
+	public User(UserRequestDTO userRequestDTO) {
+		this.name = userRequestDTO.getName();
+		this.nickname = userRequestDTO.getNickname();
+		this.account = userRequestDTO.getAccount();
+		this.password = userRequestDTO.getPassword();
+		this.age = userRequestDTO.getAge();
+		this.createdDate = LocalDateTime.now();
+		this.role = Role.USER;
+	}
+
+	/**
+	 * ADMIN 에 의해서만 변경 가능. service layer 에서 구현 예정
+	 * @param role : ADMIN, USER, BLOCK == 관리자, 사용자, 차단된사용자 3가지
+	 */
+	public void updateRole(Role role) {
+		this.role = role;
+	}
+
+	/**
+	 * 사용자 본인만 수정할 수 있는 권한이 주어져야 한다.
+	 * @param userRequestDTO : 수정될 정보들이 담겨있는 객체
+	 */
+	public void update(UserRequestDTO userRequestDTO) {
+		this.name = userRequestDTO.getName();
+		this.nickname = userRequestDTO.getNickname();
+		this.account = userRequestDTO.getAccount();
+		this.password = userRequestDTO.getPassword();
+		this.age = userRequestDTO.getAge();
+	}
 }

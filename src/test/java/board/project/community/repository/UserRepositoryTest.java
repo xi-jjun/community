e8814fad90005ce423b08a031eac2dca7b271774
xi_jjun <rlawowns97@naver.com>
2022-05-06 21:15:47 +0900

    test: 전체 데이터를 @BeforeEach로 만들고 실행시키는 것으로 변경

--- src/test/java/board/project/community/repository/UserRepositoryTest.java
+++ src/test/java/board/project/community/repository/UserRepositoryTest.java
@@ -1,11 +1,16 @@
 package board.project.community.repository;
 
+import board.project.community.controller.dto.request.UserRequestDTO;
 import board.project.community.domain.user.Role;
 import board.project.community.domain.user.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
@@ -23,289 +28,191 @@ class UserRepositoryTest {
 	@Autowired
 	private EntityManager em;
 
-	@Test
+	/**
+	 * 얘가 모든 @Test 가 실행되기 전에 실행되기 때문에 DB 에 같은 데이터가 배수로 쌓인다.
+	 * 따라서 PK find test 를 못 만드는 상황에 처함.
+	 */
+	@Transactional
+	@BeforeEach
+	void makeUsers() {
+		final String[] NAMES = {"김재현", "류도연", "윤상우", "박준영", "김근우"};
+		for (int i = 1; i < 6; i++) {
+			UserRequestDTO dto = new UserRequestDTO();
+			dto.setAccount("userAccount" + i);
+			dto.setPassword("password" + i);
+			dto.setName(NAMES[i - 1]);
+			dto.setNickname("Nickname" + i);
+			dto.setAge(26 + i);
+
+			User user = dto.toEntity();
+
+			userRepository.save(user);
+		}
+
+		for (int i = 0; i < 2; i++) {
+			UserRequestDTO dto = new UserRequestDTO();
+			dto.setAccount("userAccountBB" + i);
+			dto.setPassword("passwordBB" + i);
+			dto.setName("이름"+i);
+			dto.setNickname("blocked" + i);
+			dto.setAge(16 + i);
+
+			User user = dto.toEntity();
+			user.updateRole(Role.BLOCKED);
+
+			userRepository.save(user);
+		}
+	}
+
 	@DisplayName("사용자 정보 저장하기")
 	@Transactional
+	@Test
 	void save() {
-		System.out.println("UserRepositoryTest.save");
-		// given
-		User toBeUser = User.UserBuilder()
-				.name("김재준")
-				.role(Role.USER)
-				.credential("this is secret")
-				.password("password123")
-				.account("rlawowns97")
-				.age(26)
-				.createdDate(LocalDateTime.now())
-				.nickname("공돌이")
-				.build();
+		// given : 사용자가 회원가입하기 위해 입력한 정보는 UserRequestDTO 를 통해 정보가 전달 될 것
+		UserRequestDTO dto = new UserRequestDTO();
+		dto.setAccount("rlawowns97");
+		dto.setPassword("password123");
+		dto.setName("김재준");
+		dto.setNickname("공돌이");
+		dto.setAge(26);
+
+		User toBeUser = dto.toEntity();
+
 
 		// when
 		userRepository.save(toBeUser);
 
-		// then
-		List<User> users = userRepository.findAll();
+		em.flush();
+		em.clear();
 
-		for (User user : users) {
-			assertEquals(toBeUser.getAccount(), user.getAccount());
-			assertEquals(toBeUser.getAge(), user.getAge());
-			assertEquals(toBeUser.getId(), user.getId());
-			assertEquals(toBeUser.getName(), user.getName());
-			assertEquals(toBeUser.getNickname(), user.getNickname());
-			assertEquals(toBeUser.getCreatedDate(), user.getCreatedDate());
-			assertEquals(toBeUser.getCredential(), user.getCredential());
-			assertEquals(toBeUser.getPassword(), user.getPassword());
-		}
+
+		// then
+		User findById = userRepository.findById(toBeUser.getId());
+		User findByAccount = userRepository.findByAccount(toBeUser.getAccount());
+		User findByNickname = userRepository.findByNickname(toBeUser.getNickname());
+
+		assertEquals(findById.getNickname(), toBeUser.getNickname());
+		assertEquals(findByAccount.getNickname(), toBeUser.getNickname());
+		assertEquals(findByNickname.getNickname(), toBeUser.getNickname());
+		assertEquals(findByNickname.getAccount(), toBeUser.getAccount());
 	}
 
 	@Test
 	void findByNickname() {
-		System.out.println("UserRepositoryTest.findByName");
 		// given
-		User toBeUser = User.UserBuilder()
-				.name("김재준")
-				.role(Role.USER)
-				.credential("this is secret")
-				.password("password123")
-				.account("rlawowns97")
-				.age(26)
-				.createdDate(LocalDateTime.now())
-				.nickname("공돌이")
-				.build();
+		final String FIND_USER_NICKNAME = "Nickname2";
 
-		userRepository.save(toBeUser);
 
 		// when
-		User findUser = userRepository.findByNickname(toBeUser.getNickname());
+		User findUser = userRepository.findByNickname(FIND_USER_NICKNAME);
+
+		System.out.println(findUser.getId());
 
 		// then
-		assertEquals(toBeUser.getAccount(), findUser.getAccount());
-		assertEquals(toBeUser.getAge(), findUser.getAge());
-		assertEquals(toBeUser.getId(), findUser.getId());
-		assertEquals(toBeUser.getName(), findUser.getName());
-		assertEquals(toBeUser.getNickname(), findUser.getNickname());
-		assertEquals(toBeUser.getCreatedDate(), findUser.getCreatedDate());
-		assertEquals(toBeUser.getCredential(), findUser.getCredential());
-		assertEquals(toBeUser.getPassword(), findUser.getPassword());
+		assertEquals(findUser.getAccount(), "userAccount2");
+		assertEquals(findUser.getPassword(), "password2");
+		assertEquals(findUser.getName(), "류도연");
+		assertEquals(findUser.getNickname(), FIND_USER_NICKNAME);
+		assertEquals(findUser.getAge(), 28);
 	}
 
 	@Test
-	void findByPK() {
-		// given
-		User toBeUser = User.UserBuilder()
-				.name("김재준")
-				.role(Role.USER)
-				.credential("this is secret")
-				.password("password123")
-				.account("rlawowns97")
-				.age(26)
-				.createdDate(LocalDateTime.now())
-				.nickname("공돌이")
-				.build();
-
-		userRepository.save(toBeUser);
-		em.clear();
-
-		// when
-		User findUser = userRepository.findById(toBeUser.getId());
-
-		// then
-		assertEquals(toBeUser.getAccount(), findUser.getAccount());
-		assertEquals(toBeUser.getAge(), findUser.getAge());
-		assertEquals(toBeUser.getId(), findUser.getId());
-		assertEquals(toBeUser.getName(), findUser.getName());
-		assertEquals(toBeUser.getNickname(), findUser.getNickname());
-		assertEquals(toBeUser.getCreatedDate(), findUser.getCreatedDate());
-		assertEquals(toBeUser.getCredential(), findUser.getCredential());
-		assertEquals(toBeUser.getPassword(), findUser.getPassword());
-	}
-
-	@Test
-	@Transactional
 	void findAllActiveUsers() {
 		// given
-		final int ACTIVE_USERS = 3;
-		final int BLOCKED_USERS = 2;
-		// active users
-		for (int i = 0; i < ACTIVE_USERS; i++) {
-			User toBeUser = User.UserBuilder()
-					.name("김재준" + i)
-					.role(Role.USER)
-					.credential("this is secret")
-					.password("password123")
-					.account("rlawowns" + i)
-					.age(26)
-					.createdDate(LocalDateTime.now())
-					.nickname("공돌이" + i)
-					.build();
-			userRepository.save(toBeUser);
-		}
+		final int BLOCKED_USER_NUMBER = 2;
 
-		// blocked users
-		for (int i = 0; i < BLOCKED_USERS; i++) {
-			User toBeUser = User.UserBuilder()
-					.name("차단당한 사람" + i)
-					.role(Role.BLOCKED)
-					.credential("this is secret")
-					.password("password123")
-					.account("rlawowns" + (i + 5))
-					.age(26)
-					.createdDate(LocalDateTime.now())
-					.nickname("공돌이" + (i + 5))
-					.build();
-			userRepository.save(toBeUser);
-		}
-
-		em.clear();
 
 		// when
+		List<User> users = userRepository.findAll();
 		List<User> allActiveUsers = userRepository.findAllActiveUsers();
 
+
 		// then
-		assertEquals(ACTIVE_USERS, allActiveUsers.size());
+		System.out.println(users.size());
+		assertEquals(users.size() - BLOCKED_USER_NUMBER, allActiveUsers.size());
 	}
 
 	@Test
-	@Transactional
 	void findByAccount() {
 		// given
-		final String ACCOUNT = "rlawowns97";
-		User toBeUser = User.UserBuilder()
-				.name("김재준")
-				.role(Role.USER)
-				.credential("this is secret")
-				.password("password123")
-				.account(ACCOUNT)
-				.age(26)
-				.createdDate(LocalDateTime.now())
-				.nickname("공돌이")
-				.build();
+		final String FIND_ACCOUNT = "userAccount4";
 
-		userRepository.save(toBeUser);
 
 		// when
-		User findUser = userRepository.findByAccount(ACCOUNT);
+		User findUserByAccount = userRepository.findByAccount(FIND_ACCOUNT);
+
 
 		// then
-		assertEquals(toBeUser.getAccount(), findUser.getAccount());
-		assertEquals(toBeUser.getAge(), findUser.getAge());
-		assertEquals(toBeUser.getId(), findUser.getId());
-		assertEquals(toBeUser.getName(), findUser.getName());
-		assertEquals(toBeUser.getNickname(), findUser.getNickname());
-		assertEquals(toBeUser.getCreatedDate(), findUser.getCreatedDate());
-		assertEquals(toBeUser.getCredential(), findUser.getCredential());
-		assertEquals(toBeUser.getPassword(), findUser.getPassword());
+		assertEquals(findUserByAccount.getAccount(), FIND_ACCOUNT);
+		assertEquals(findUserByAccount.getPassword(), "password4");
+		assertEquals(findUserByAccount.getName(), "박준영");
+		assertEquals(findUserByAccount.getNickname(), "Nickname4");
+		assertEquals(findUserByAccount.getAge(), 30);
 	}
 
 	@Test
 	void findAll() {
 		// given
-		final int ACTIVE_USERS = 3;
-		final int BLOCKED_USERS = 2;
-		final int ALL_USERS = ACTIVE_USERS + BLOCKED_USERS;
-		// active users
-		for (int i = 0; i < ACTIVE_USERS; i++) {
-			User toBeUser = User.UserBuilder()
-					.name("김재준" + i)
-					.role(Role.USER)
-					.credential("this is secret")
-					.password("password123")
-					.account("rlawowns" + i)
-					.age(26)
-					.createdDate(LocalDateTime.now())
-					.nickname("공돌이" + i)
-					.build();
-			userRepository.save(toBeUser);
-		}
+		final int ALL_USERS = 7;
 
-		// blocked users
-		for (int i = 0; i < BLOCKED_USERS; i++) {
-			User toBeUser = User.UserBuilder()
-					.name("차단당한 사람" + i)
-					.role(Role.BLOCKED)
-					.credential("this is secret")
-					.password("password123")
-					.account("rlawowns" + (i + 5))
-					.age(26)
-					.createdDate(LocalDateTime.now())
-					.nickname("공돌이" + (i + 5))
-					.build();
-			userRepository.save(toBeUser);
-		}
 
 		// when
 		List<User> users = userRepository.findAll();
+
 
 		// then
 		assertEquals(ALL_USERS, users.size());
 	}
 
-	@Test
 	@Transactional
-	void update() {
-		// given
-		User toBeUser = User.UserBuilder()
-				.name("김재준")
-				.role(Role.USER)
-				.credential("this is secret")
-				.password("password123")
-				.account("rlawowns97")
-				.age(26)
-				.createdDate(LocalDateTime.now())
-				.nickname("공돌이")
-				.build();
+	@Test
+	void updateUserInfo() {
+		// given : update 할 정보를 client 에게 받아서 서버로 가져온다. 그 때 UserRequestDTO 에 담아서 가져온다.
+		UserRequestDTO updateDto = new UserRequestDTO();
+		updateDto.setAccount("rlawowns97");
+		updateDto.setPassword("password123");
+		updateDto.setName("수정된 이름");
+		updateDto.setNickname("공돌이");
+		updateDto.setAge(26);
 
-//		userRepository.save(toBeUser);
-		em.persist(toBeUser);
-		em.flush();
-		em.clear();
-
-		User findUser = userRepository.findById(toBeUser.getId());
-		findUser.setName("수정된 이름");
-
-		em.flush();
-		em.clear();
+		final String USER_NICKNAME_IN_SESSION = "Nickname2";
 
 		// when
-		userRepository.update(findUser);
+		User findUser = userRepository.findByNickname(USER_NICKNAME_IN_SESSION);
+		findUser.update(updateDto);
+		em.flush();
+		em.clear();
+
 
 		// then
-		User result = userRepository.findById(toBeUser.getId());
+		User result = userRepository.findById(findUser.getId());
+		assertEquals(findUser.getPassword(), result.getPassword());
+		assertEquals(findUser.getAge(), result.getAge());
 		assertEquals(findUser.getName(), result.getName());
-		System.out.println(result.getName());
+		assertEquals(findUser.getNickname(), result.getNickname());
+		assertEquals(findUser.getAccount(), result.getAccount());
+		assertEquals(findUser.getRole(), result.getRole());
+		assertEquals(findUser.getCreatedDate(), result.getCreatedDate());
 	}
 
-	@Test
 	@Transactional
+	@Test
 	void remove() {
 		// given
-		final int ACTIVE_USERS = 3;
-		for (int i = 0; i < ACTIVE_USERS; i++) {
-			User toBeUser = User.UserBuilder()
-					.name("김재준" + i)
-					.role(Role.USER)
-					.credential("this is secret")
-					.password("password123")
-					.account("rlawowns" + i)
-					.age(26)
-					.createdDate(LocalDateTime.now())
-					.nickname("공돌이" + i)
-					.build();
-			userRepository.save(toBeUser);
-		}
+		final String REMOVED_USER_NICKNAME = "Nickname3";
 
-		em.clear();
 
 		// when
-		final String toBeErasedUserNickname = "공돌이2";
-		User findUser = userRepository.findByNickname(toBeErasedUserNickname);
+		User findUser = userRepository.findByNickname(REMOVED_USER_NICKNAME);
+		Long findId = findUser.getId();
 		userRepository.remove(findUser);
 
+		em.flush();
+		em.clear();
+
+
 		// then
-		List<User> users = userRepository.findAll();
-		assertEquals(ACTIVE_USERS - 1, users.size());
-		for (User user : users) {
-			System.out.println("user.getNickname() = " + user.getNickname());
-		}
+		User removedUser = userRepository.findById(findId);
+		assertNull(removedUser);
 	}
 }
